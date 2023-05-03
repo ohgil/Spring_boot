96b8193d0fede0dbfe42838f2902e6fb25dd7af8
ohgil <ohgilhwan1957@gmail.com>
2023-05-03 11:19:18 +0900

    5월 3일 수업

--- sbb/src/main/java/com/example/sbb/QuestionRepository.java
+++ sbb/src/main/java/com/example/sbb/QuestionRepository.java
@@ -2,5 +2,6 @@ package com.example.sbb;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String subject);
 
 }//QuestionRepository는 jpa에서 받아온다
--- sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -5,25 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
-	@Autowired
-private QuestionRepository questionRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
 
-	@Test
-	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
+    @Test
+    void testJpa() {
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
 
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
-	}
+    }//test쪽으로 따로 뺀 다음에 실행시키고, 다시 Sbb로 돌아가서 실행시키고, h2에 잘 입력됐는지 확인
 
 }
