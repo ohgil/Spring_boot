b0891843adcf5be00a3e60fcf856f2f20b1242f1
ohgil <ohgilhwan1957@gmail.com>
2023-05-03 12:34:46 +0900

    5월 3일 수업

--- sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -19,15 +19,10 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(2);//questionRepository에서 Id2 객체로 가져옴
-        assertTrue(oq.isPresent());//존재하는지 체크
-        Question q = oq.get();//존재하면 q에 값을 담음
-
-        Answer a = new Answer();
-        a.setContent("네 자동으로 생성됩니다.");//content에 입력되는 값
-        a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
-        a.setCreateDate(LocalDateTime.now());//createdate에 답변달린 시간을 입력
-        this.answerRepository.save(a);//answerRepository에 저장
+        Optional<Answer> oa = this.answerRepository.findById(1);//answerRepository Id 1번값을 가져옴.
+        assertTrue(oa.isPresent());//조건 맞으면
+        Answer a = oa.get();//객체에 통으로 값을 담는다
+        assertEquals(2, a.getQuestion().getId());//그리고 a객체의 question_id가 2인지 확인
 
     }
 
