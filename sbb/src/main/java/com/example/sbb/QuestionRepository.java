05b28c597f7c1080bb8b7179a8306eb479be0a85
ohgil <ohgilhwan1957@gmail.com>
2023-05-03 09:42:22 +0900

    5월 3일 수업

--- sbb/src/main/java/com/example/sbb/QuestionRepository.java
+++ sbb/src/main/java/com/example/sbb/QuestionRepository.java
@@ -3,4 +3,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
-}
+}//QuestionRepository는 jpa에서 받아온다
