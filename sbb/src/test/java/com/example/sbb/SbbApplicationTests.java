bb450f895533fb49959c5e85a433f8c54d9474e7
ohgil <ohgilhwan1957@gmail.com>
2023-05-03 12:20:43 +0900

    5월 3일 수업

--- sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -4,20 +4,30 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
+
     @Test
     void testJpa() {
-        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-        Question q = qList.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
+        Optional<Question> oq = this.questionRepository.findById(2);//questionRepository에서 Id2 객체로 가져옴
+        assertTrue(oq.isPresent());//존재하는지 체크
+        Question q = oq.get();//존재하면 q에 값을 담음
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");//content에 입력되는 값
+        a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+        a.setCreateDate(LocalDateTime.now());//createdate에 답변달린 시간을 입력
+        this.answerRepository.save(a);//answerRepository에 저장
 
     }
 
