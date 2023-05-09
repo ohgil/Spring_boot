fdc181b336c34f4124674913248e610cffd19569
ohgil <ohgilhwan1957@gmail.com>
2023-05-09 11:43:22 +0900

    5월 9일 수업

--- sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -7,6 +7,7 @@ import com.example.sbb.Answer.Answer;
 import com.example.sbb.Answer.AnswerRepository;
 import com.example.sbb.Question.Question;
 import com.example.sbb.Question.QuestionRepository;
+import com.example.sbb.Question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,21 +20,14 @@ import static org.testng.AssertJUnit.assertTrue;
 class SbbApplicationTests {
 
     @Autowired
-    private QuestionRepository questionRepository;
-
-    @Autowired
-    private AnswerRepository answerRepository;
+    private QuestionService questionService;
 
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        Answer a = new Answer();
-        a.setContent("네 자동으로 생성됩니다.");
-        a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
-        a.setCreateDate(LocalDateTime.now());
-        this.answerRepository.save(a);
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
     }
 }
